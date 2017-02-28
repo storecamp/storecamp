c2c0d3f0bd45e8d6a4215ec89ada6a51f9546e9b
Иван Юрьевич Николенко <ni_cole@i.ua>
2017-03-01 00:28:55 +0200

    slider changed owl.carousel

--- resources/views/site/partials/slider.blade.php
+++ resources/views/site/partials/slider.blade.php
@@ -10,74 +10,51 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="carousel carousel-most-viewed slide" id="itemslider">
-                <div class="carousel-inner">
-                    <?php $countViewed = count($mostViewed); ?>
-                    @forelse($mostViewed as $product)
-                        @if ($loop->first)
-                            <div class="item active">
-                                <div class="col-xs-12 col-sm-6 col-md-2">
-                                    <a target="_blank" href="{{route('site::products::show', $product->unique_id)}}">
-                                        <?php $mainImage = $product->getMedia('gallery')->first(); ?>
-                                        <img src="{{$mainImage ? $mainImage->getUrl() : asset("/img/Image-not-found.gif")}}"
-                                             class="img-responsive center-block"/>
-                                    </a>
-                                    <h4 class="text-center">{{$product->title}}</h4>
-                                    <h5 class="text-center">{{shopFormat($product->price)}}</h5>
-                                </div>
-                            </div>
-                        @else
-                            <div class="item">
-                                <div class="col-xs-12 col-sm-6 col-md-2">
-                                    <a target="_blank" href="{{route('site::products::show', $product->unique_id)}}">
-                                        <?php $mainImage = $product->getMedia('gallery')->first(); ?>
-                                        <img src="{{$mainImage ? $mainImage->getUrl() : asset("/img/Image-not-found.gif")}}"
-                                             class="img-responsive center-block"/>
-                                    </a>
-                                    <h4 class="text-center">{{$product->title}}</h4>
-                                    <h5 class="text-center">{{shopFormat($product->price)}}</h5>
-                                </div>
-                            </div>
-                        @endif
-                    @empty
+            <div class="owl-carousel owl-theme">
+                <?php $countViewed = count($mostViewed); ?>
+                @forelse($mostViewed as $product)
+                    <div class="">
+                        <a target="_blank" href="{{route('site::products::show', $product->unique_id)}}">
+                            <?php $mainImage = $product->getMedia('gallery')->first(); ?>
+                            <img src="{{$mainImage ? $mainImage->getUrl() : asset("/img/Image-not-found.gif")}}"
+                                 class="img-responsive center-block"/>
+                        </a>
+                        <h4 class="text-center">
+                            <a target="_blank" href="{{route('site::products::show', $product->unique_id)}}">
+                                {{$product->title}}
+                            </a>
+                        </h4>
+                        <h5 class="text-center">{{shopFormat($product->price)}}</h5>
+                    </div>
+                @empty
+                    <div>
                         <h3 class="text-muted">No More Products In This Category</h3>
-                    @endforelse
-                </div>
-                <div id="slider-control">
-                    <a class="left carousel-control" href="#itemslider" data-slide="prev"><img
-                                src="https://s12.postimg.org/uj3ffq90d/arrow_left.png" alt="Left"
-                                class="img-responsive"></a>
-                    <a class="right carousel-control" href="#itemslider" data-slide="next"><img
-                                src="https://s12.postimg.org/djuh0gxst/arrow_right.png" alt="Right"
-                                class="img-responsive"></a>
-                </div>
+                    </div>
+                @endforelse
             </div>
         </div>
     </div>
+
 </div>
 <!-- Item slider end-->
-
 @push('scripts-add_on')
+<link rel="stylesheet" href="{{asset('plugins/owl.carousel/dist/assets/owl.carousel.min.css')}}"/>
+<script src="{{asset('plugins/owl.carousel/dist/owl.carousel.min.js')}}"></script>
 <script>
-    (function () {
-
-        $('#itemslider').carousel({interval: 3000});
-    }());
-
-    (function () {
-        $('.carousel-most-viewed .item').each(function () {
-            var itemToClone = $(this);
-
-            for (var i = 1; i < {!!$countViewed!!}; i++) {
-                itemToClone = itemToClone.next();
-                if (!itemToClone.length) {
-                    itemToClone = $(this).siblings(':first');
-                }
-                itemToClone.children(':first-child').clone()
-                    .addClass("cloneditem-" + (i))
-                    .appendTo($(this));
-            }
-        });
-    }());
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+    $('.play').on('click', function () {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+        owl.trigger('stop.owl.autoplay')
+    })
 </script>
 @endpush
