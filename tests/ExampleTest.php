f406be7aeadacbf75d9beed5ba02fe3cd90b9be3
Иван Юрьевич Николенко <ni_cole@i.ua>
2017-03-22 11:00:15 +0200

    test exmple remove test dummy test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,17 +3,5 @@
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
-    {
-        $this->artisan('migrate');
-        $this->artisan('db:seed');
 
-        $this->visit('/home')
-             ->see('Navigation');
-    }
 }
